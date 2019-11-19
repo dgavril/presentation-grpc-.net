e9efc52a2c7d966cfe97f97cc74f3a5901b94467
Daniel Gavril <daniel.gavril@centric.eu>
2019-11-20 00:06:37 +0200

    First proto check in.

---
+++ GeoLocation/GeoLocation.ConsoleClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using GeoLocation.Protos;
+using Grpc.Net.Client;
+
+namespace GeoLocation.ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var result = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Daniel"
+            });
+
+            Console.WriteLine(result.Message);
+            Console.ReadKey();
+        }
+    }
+}
